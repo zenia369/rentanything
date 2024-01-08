c109fef31ffa2a8c4c72e6e198fa54b999a403f2
Kushnir E. <kyshnirxeneea@gmail.com>
2024-01-08 18:15:48 +0200

    build: add react-icon package

--- remix.config.js
+++ remix.config.js
@@ -2,6 +2,7 @@
 export default {
   ignoredRouteFiles: ["**/.*"],
   tailwind: true,
+  serverDependenciesToBundle: [/^react-icons/],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
